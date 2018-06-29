aa630f22f582ce3492737c3a163bc7b13cd63a88
Elb4t <eloy4t@gmail.com>
2018-06-29 09:26:51 +0200

    Suscripción para control remoto del LED

--- app/src/main/java/es/elb4t/mqtt/MainActivity.kt
+++ app/src/main/java/es/elb4t/mqtt/MainActivity.kt
@@ -3,11 +3,11 @@ package es.elb4t.mqtt
 import android.app.Activity
 import android.os.Bundle
 import android.util.Log
-import org.eclipse.paho.client.mqttv3.MqttClient
-import org.eclipse.paho.client.mqttv3.MqttException
-import org.eclipse.paho.client.mqttv3.MqttMessage
+import com.google.android.things.pio.Gpio
+import com.google.android.things.pio.PeripheralManager
+import org.eclipse.paho.client.mqttv3.*
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence
-
+import java.io.IOException
 
 
 /**
@@ -30,33 +30,100 @@ import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence
  * @see <a href="https://github.com/androidthings/contrib-drivers#readme">https://github.com/androidthings/contrib-drivers#readme</a>
  *
  */
-class MainActivity : Activity() {
+class MainActivity : Activity(), MqttCallback {
 
     companion object {
         private val TAG = "Things"
         private val topic = "elllabel/test"
-        private val hello = "Hello world!"
+        private val hello = "Hello world! Android Things conectada."
         private val qos = 1
         private val broker = "tcp://iot.eclipse.org:1883"
         private val clientId = "lens_M8gAXD2AXDuhTkq8y7XzrW6yDkQ"
+        private val topic_gestion = "elllabel/gestion"
+        private val topic_led = "elllabel/led"
     }
+
+    private val PIN_LED = "BCM18"
+    var mLedGpio: Gpio? = null
+    lateinit var client: MqttClient
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val service = PeripheralManager.getInstance()
         try {
-            val client = MqttClient(broker, clientId, MemoryPersistence())
+            mLedGpio = service.openGpio(PIN_LED)
+            mLedGpio?.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW)
+        } catch (e: IOException) {
+            Log.e(TAG, "Error en el API PeripheralIO", e)
+        }
+
+        try {
+            val clientId = MqttClient.generateClientId()
+            client = MqttClient(broker, clientId, MemoryPersistence())
+            client.setCallback(this)
+            val connOpts = MqttConnectOptions()
+            connOpts.isCleanSession = true
+            connOpts.keepAliveInterval = 60
+            connOpts.setWill(topic_gestion, "Android Things desconectada!"
+                    .toByteArray(), qos, false)
             Log.i(TAG, "Conectando al broker $broker")
-            client.connect()
+            client.connect(connOpts)
             Log.i(TAG, "Conectado")
             Log.i(TAG, "Publicando mensaje: $hello")
             val message = MqttMessage(hello.toByteArray())
             message.qos = qos
-            client.publish(topic, message)
+            client.publish(topic_gestion, message)
             Log.i(TAG, "Mensaje publicado")
-            client.disconnect()
-            Log.i(TAG, "Desconectado")
+            client.subscribe(topic_led, qos)
+            Log.i(TAG, "Suscrito a $topic_led")
         } catch (e: MqttException) {
             Log.e(TAG, "Error en MQTT.", e)
         }
-
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        try {
+            if (client != null && client.isConnected) {
+                client.disconnect()
+            }
+        } catch (e: MqttException) {
+            Log.e(TAG, "Error en MQTT.", e)
+        }
+        if (mLedGpio != null) {
+            try {
+                mLedGpio?.close()
+            } catch (e: IOException) {
+                Log.e(TAG, "Error en el API PeripheralIO", e)
+            } finally {
+                mLedGpio = null
+            }
+        }
+    }
+
+    override fun messageArrived(topic: String, message: MqttMessage) {
+        val payload = String(message.payload)
+        Log.d(TAG, payload)
+        when (payload) {
+            "ON" -> {
+                mLedGpio?.value = true
+                Log.d(TAG, "LED ON!")
+            }
+            "OFF" -> {
+                mLedGpio?.value = false
+                Log.d(TAG, "LED OFF!")
+            }
+            else -> Log.d(TAG, "Comando no soportado")
+        }
+    }
+
+    override fun connectionLost(cause: Throwable?) {
+        Log.d(TAG, "Conexión perdida...")
+    }
+
+    override fun deliveryComplete(token: IMqttDeliveryToken?) {
+        Log.d(TAG, "Entrega completa!")
+    }
+
 }
